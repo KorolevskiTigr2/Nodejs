13749018ffd4229dd89293dd6ff96c71ff67dda5
Alexander Medvedev <mega.medved87@gmail.com>
2024-08-15 11:54:32 -0500

    Routing requests

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req,res) =>{
-    console.log(req.url, req.method, req.headers)
-    // process.exit();
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head>');
+          res.write('<title>My First Page</title>')
+        res.write('</head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button tupe="submit">Send</button></form></body>')
+     res.write('</html>');
+      return res.end();
+    }
+    
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
         res.write('<head>');
