91dc2b35b0ee81f6cfe36708c720cad16680467d
Alexander Medvedev <mega.medved87@gmail.com>
2024-08-15 11:45:25 -0500

    Request & Response Headers

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-
 const http = require('http');
 
 const server = http.createServer((req,res) =>{
-    console.log('req')
-    process.exit();
+    console.log(req.url, req.method, req.headers)
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+        res.write('<head>');
+          res.write('<title>My First Page</title>')
+        res.write('</head>');
+        res.write('<body><h1>Hello there!</h1></body>')
+     res.write('</html>');
+
+     res.end();
 });
 
-server.listen(3003)
+server.listen(3000);
